c688f735f9930aed14999901e3a39ee16d51e77e
ronglei <123456>
2019-11-16 17:03:51 +0800

    第一次提交

---
+++ src/TestFirst.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright: Copyright (c) 2019 Asiainfo
+ *
+ * @ClassName: PACKAGE_NAME
+ * @Description: 该类的功能描述
+ * @version: v1.0.0
+ * @author: lenovo
+ * @date: 2019/11/16 17:03
+ * <p>
+ * Modification History:
+ * Date         Author          Version            Description
+ * ---------------------------------------------------------*
+ * 2019/11/16     lenovo           v1.0.0               修改原因
+ */
+public class TestFirst {
+    public static void main(String[] args) {
+        System.out.println("Hello TestFirst!");
+    }
+}
