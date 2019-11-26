3fafe77af5246ebf909f4f51141b0e74aaa32282
ronglei <123456>
2019-11-26 23:18:51 +0800

    bug分支测试第二次

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("第一次修改");
         System.out.println("我提交的一个bug分支");
+        System.out.println("我提交的第二个bug分支");
     }
 }
